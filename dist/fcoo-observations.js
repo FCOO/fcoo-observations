5483543e62953e91677d3f2823336a519b9554cb
Niels Holt <niels@steenbuchholt.dk>
2022-01-29 00:05:27 +0100

    Fine tuning of observation icons

--- dist/fcoo-observations.js
+++ dist/fcoo-observations.js
@@ -45,7 +45,7 @@
     ns.FCOOObservations = function(options){
         var _this = this;
         this.options = $.extend(true, {}, {
-			VERSION         : "3.4.5",
+			VERSION         : "3.4.6",
             subDir          : {
                 observations: 'observations',
                 forecasts   : 'forecasts'
