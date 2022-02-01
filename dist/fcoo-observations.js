fcad0ddf903a3a8d7ebabdabf7ded3206500b9fc
Niels Holt <niels@steenbuchholt.dk>
2022-02-02 00:18:34 +0100

    Update dependencies gitlab to github

--- dist/fcoo-observations.js
+++ dist/fcoo-observations.js
@@ -45,7 +45,7 @@
     ns.FCOOObservations = function(options){
         var _this = this;
         this.options = $.extend(true, {}, {
-			VERSION         : "3.5.3",
+			VERSION         : "3.5.4",
             subDir          : {
                 observations: 'observations',
                 forecasts   : 'forecasts'
