0cbeab347f19ae866f471508ba89875eec0c8a00
Niels Holt <niels@steenbuchholt.dk>
2022-03-29 12:05:50 +0200

    Demo updated with sealvl-test

--- dist/fcoo-observations.js
+++ dist/fcoo-observations.js
@@ -45,7 +45,7 @@
     ns.FCOOObservations = function(options){
         var _this = this;
         this.options = $.extend(true, {}, {
-			VERSION         : "3.7.0",
+			VERSION         : "3.7.1",
             subDir          : {
                 observations: 'observations',
                 forecasts   : 'forecasts'
